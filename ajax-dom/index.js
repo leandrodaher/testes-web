e7697d49c03017c00fdb8e12e03d5499e9452aa6
Leandro Daher <leandrodaher96@gmail.com>
2020-07-23 13:22:45 -0300

    Update index.js

--- ajax-dom/index.js
+++ ajax-dom/index.js
@@ -7,3 +7,24 @@ var load = function() {
 }
 
 document.getElementById("btn").addEventListener("click", load);
+
+/* *************************** */
+/*
+import { Teste } from './components/teste.js';
+import * as $ from './components/ajax.js';
+
+var p = new Teste("#app");
+
+var load = function() {
+    p.getData();
+    setTimeout(()=>{console.log(p.data)}, 5000);
+}
+
+var load2 = function() {
+    console.log(p.data);
+}
+
+document.getElementById("btn1").addEventListener("click", load);
+document.getElementById("btn2").addEventListener("click", load2);
+*/
+/* *************************** */
