eccffaf7d196ed37ab2e6bfb869058d3ee23dfc3
Leandro Daher <leandrodaher96@gmail.com>
2020-07-23 13:20:59 -0300

    Update teste.js

--- ajax-dom/components/teste.js
+++ ajax-dom/components/teste.js
@@ -22,5 +22,31 @@ export class Teste {
             })
         }, 3000)
     }
+    
+   /*
+   getData() {
+        document.querySelector(this.container).innerHTML = "Loading...";
+        //atraso proposital 3s
+        setTimeout(()=>{
+            $.Ajax({
+                url:"http://worldtimeapi.org/api/timezone",//request URL
+                type:"GET",
+                method: true,
+                data:{
+                
+                },
+                success: function(data) {
+                    let element = document.querySelector(this.container);
+                    this.data = JSON.parse(data);
+
+                    element.innerHTML = '';
+                    this.data.forEach(item => {
+                        element.innerHTML += `<li>${item}</li>`;
+                    });
+                }.bind(this) // para acessar a propiedade this.container
+            })
+        }, 3000)
+    }
+    */
 
 }
